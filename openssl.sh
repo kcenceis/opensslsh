538abee82d3fa4de4024dc42f3a5821822bd5399
kcenceis <25601736a@gmail.com>
2020-09-13 23:11:00 +0800

    Update openssl.sh
    fix

--- openssl.sh
+++ openssl.sh
@@ -36,13 +36,28 @@ fi
 if [ ! -d "/usr/lib64/libssl.so.bak" ]; then
   rm -rf /usr/lib64/libssl.so.bak
 fi
+if [ ! -d "/usr/lib64/libcrypto.so.bak" ]; then
+  rm -rf /usr/lib64/libssl.so.bak
+fi
+if [ ! -d "/usr/lib64/libcrypto.so.1.1.bak" ]; then
+  rm -rf /usr/lib64/libssl.so.bak
+fi
+if [ ! -d "/usr/lib64/libssl.so.1.1.bak" ]; then
+  rm -rf /usr/lib64/libssl.so.bak
+fi
 
 mv /usr/bin/openssl /usr/bin/openssl.bak
 mv /usr/include/openssl /usr/include/openssl.bak
 mv /usr/lib64/libssl.so /usr/lib64/libssl.so.bak
+mv /usr/lib64/libcrypto.so /usr/lib64/libcrypto.so.bak
+mv /usr/lib64/libssl.so.1.1 /usr/lib64/libssl.so.1.1.bak
+mv /usr/lib64/libcrypto.so.1.1 /usr/lib64/libcrypto.so.1.1.bak
 ln -s /usr/local/openssl/.openssl/bin/openssl /usr/bin/openssl  
 ln -s /usr/local/openssl/.openssl/include/openssl /usr/include/openssl
 ln -s /usr/local/openssl/.openssl/lib/libssl.so /usr/lib64/libssl.so
+ln -s /usr/local/openssl/.openssl/lib/libcrypto.so /usr/lib64/libcrypto.so
+ln -s /usr/local/openssl/.openssl/lib/libcrypto.so.1.1 /usr/lib64/libcrypto.so.1.1
+ln -s /usr/local/openssl/.openssl/lib/libssl.so.1.1 /usr/lib64/libssl.so.1.1
 ldconfig -v
 
 openssl version -a
